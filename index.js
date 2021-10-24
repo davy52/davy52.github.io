8de640afff46c95fc03d1f622d3ac6bf9c081c6a
dutchi13 <59352297+dutchi13@users.noreply.github.com>
2021-10-24 18:23:39 +0200

    circle

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
+// Where is the circle
+let x, y, X, Y, i = 0;
+
 function setup() {
-	createCanvas(400, 400);
+  createCanvas(720, 400);
+  // Starts in the middle
+  x = width / 2;
+  y = height / 2;
 }
 
 function draw() {
-	rect(100,100,20,20);
+  background(200);
+  
+  // Draw a circle
+  stroke(50);
+  fill(100);
+  ellipse(X,  Y, 24, 24);
+  
+  // Jiggling randomly on the horizontal axis
+  X = x + 100 *  sin(i * 0.025);
+  // Moving up at a constant speed
+  Y = y + 100 * sin(i * 0.025 + PI/2);
+  i++; 
+  // Reset to the bottom
+  if (y < 0) {
+    y = height;
+  }
 }
