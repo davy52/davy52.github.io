47edf4b4ee58ea83c5ae64b023fbd8b1de46e80d
dutchi13 <59352297+dutchi13@users.noreply.github.com>
2021-10-25 00:19:23 +0200

    Revert "dev1"
    This reverts commit 09fe4d9c852748a9481deb9812ec2e54668fe240.

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
 // Where is the circle
-let p;
+let x, y, X, Y, i = 0;
 
 function setup() {
-  	createCanvas(720, 720);
-  	background(200);
-	
-	drawAxis();
-	p = new particle(300, 300, 1, 0.5, 20);
-	p.showColor(color(31, 219, 169));
+  createCanvas(720, 400);
+  // Starts in the middle
+  x = width / 2;
+  y = height / 2;
 }
 
 function draw() {
-	p.move();
-	p.showColor(color(31, 219, 169));
-}
+  background(200);
+  
+  // Draw a circle
+  stroke(50);
+  fill(100);
+  ellipse(X,  Y, 24, 24);
+  
+  // Jiggling randomly on the horizontal axis
+  X = x + 100 *  sin(i * 0.025);
+  // Moving up at a constant speed
+  Y = y + 100 * sin(i * 0.025 + PI/2);
 
-function nFractal(z0, z1, z2, zt) {
-
-}
-
-function drawAxis() {
-	strokeWeight(2);
-	stroke(19, 66, 63);
-	line(0, height/2, width, height/2);
-	line(width/2, 0, width/2, height);
+  i++; 
+  // Reset to the bottom
+  if (y < 0) {
+    y = height;
+  }
 }
