548c1c529bafdac75e1a05ee259df85e50d0453e
dutchi13 <59352297+dutchi13@users.noreply.github.com>
2021-10-24 15:23:05 +0200

    message:)

---
+++ index.js
@@ -0,0 +1,13 @@
+function setup() {
+	createCanvas(400, 400);
+}
+
+function draw() {
+/*	if (mouseIsPressed) {
+		fill(0);
+	} else {
+		fill(255);
+	}*/
+	fill(255);
+	ellipse(nouseX, mouseY, 80, 80);
+}
